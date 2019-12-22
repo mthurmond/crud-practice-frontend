8165a36399b8fa4492d05a1b36967a07f695620c
Matt T <matt.thurmond@gmail.com>
2019-12-22 11:51:06 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import ModalForm from './Components/Modals/Modal'
 import DataTable from './Components/Tables/DataTable'
-import { CSVLink } from "react-csv"
+import { CSVLink } from 'react-csv'
 
 class App extends Component {
   state = {
